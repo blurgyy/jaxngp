3d805f4d918b1692a9828ffb8b9fda9cb710c440
Gaoyang Zhang <gy@blurgy.xyz>
2023-03-26 01:23:43 +0800

    utils: add metrics subcommand and update CLI args handling
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- utils/__main__.py
+++ utils/__main__.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from functools import partial, reduce
 import os
 from pathlib import Path
-from typing import Annotated, Tuple, Union
+from typing import Annotated, List, Tuple, Union
 from typing_extensions import assert_never
 
 from PIL import Image
@@ -12,11 +12,12 @@ import numpy as np
 import tyro
 
 from utils.common import setup_logging
-from utils.data import add_border, blend_alpha_channel, side_by_side
+from utils.data import add_border, blend_alpha_channel, psnr, side_by_side
 
 
 @dataclass(frozen=True, kw_only=True)
 class Concatenate:
+    image_paths: tyro.conf.Positional[List[Path]]
     # output image save path, the path will be overwritten with a warning
     out: Path
     # if specified, concatenate vertically instead of horizontally
@@ -30,7 +31,10 @@ class Concatenate:
 
 @dataclass(frozen=True, kw_only=True)
 class Metrics:
+    gt: Path
+    image_paths: tyro.conf.Positional[List[Path]]
     psnr: bool=True
+    use_white_bg: bool=True
 
 
 Args = Union[
@@ -51,49 +55,56 @@ Args = Union[
 ]
 
 
-def main(_args: Tuple[Args, list[str]]):
-    args, extra_cli_args = _args
+def main(args: Args):
     logger = setup_logging("utils", "INFO")
-    do = args.do
-    if isinstance(do, Concatenate):
-        if do.out.is_dir():
-            logger.error("output path '{}' is a directory".format(do.out))
+    if isinstance(args, Concatenate):
+        if args.out.is_dir():
+            logger.error("output path '{}' is a directory".format(args.out))
             exit(1)
-        if do.out.exists():
-            logger.warn("output path '{}' exists and will be overwritten".format(do.out))
-            if not os.access(do.out, os.W_OK):
-                logger.error("output path '{}' is readonly".format(do.out))
+        if args.out.exists():
+            logger.warn("output path '{}' exists and will be overwritten".format(args.out))
+            if not os.access(args.out, os.W_OK):
+                logger.error("output path '{}' is readonly".format(args.out))
                 exit(2)
-        if do.out.suffix.lower() not in map(lambda x: "." + x, ["jpg", "jpeg", "png", "tif", "tiff", "bmp", "webp"]):
-            logger.warn("the file extension '{}' might not be supported".format(do.out.suffix))
+        if args.out.suffix.lower() not in map(lambda x: "." + x, ["jpg", "jpeg", "png", "tif", "tiff", "bmp", "webp"]):
+            logger.warn("the file extension '{}' might not be supported".format(args.out.suffix))
 
         images = list(map(
-            lambda img: blend_alpha_channel(img, use_white_bg=do.use_white_bg) if img.shape[-1] == 4 else img,
-            map(np.asarray, map(Image.open, extra_cli_args)),
+            lambda img: blend_alpha_channel(img, use_white_bg=args.use_white_bg) if img.shape[-1] == 4 else img,
+            map(np.asarray, map(Image.open, args.image_paths)),
         ))
         H, W = images[0].shape[:2]
         oimg = reduce(
             partial(
                 side_by_side,
-                H=(None if do.vertical else H),
-                W=(W if do.vertical else None),
-                vertical=do.vertical,
-                gap=do.gap,
+                H=(None if args.vertical else H),
+                W=(W if args.vertical else None),
+                vertical=args.vertical,
+                gap=args.gap,
             ),
             images,
         )
-        oimg = add_border(oimg, width=do.border)
+        oimg = add_border(oimg, width=args.border)
         logger.info("saving image ...")
-        Image.fromarray(np.asarray(oimg)).save(do.out)
-        logger.info("image ({}x{}) saved to '{}'".format(oimg.shape[1], oimg.shape[0], do.out))
+        Image.fromarray(np.asarray(oimg)).save(args.out)
+        logger.info("image ({}x{}) saved to '{}'".format(oimg.shape[1], oimg.shape[0], args.out))
 
-    elif isinstance(do, Metrics):
-        raise NotImplementedError("Metrics is not implemented")
+    elif isinstance(args, Metrics):
+        gt_image = np.asarray(Image.open(args.gt))
+        if gt_image.shape[-1] == 4:
+            gt_image = blend_alpha_channel(gt_image, use_white_bg=args.use_white_bg)
+        images = list(map(
+            lambda img: blend_alpha_channel(img, use_white_bg=args.use_white_bg) if img.shape[-1] == 4 else img,
+            map(np.asarray, map(Image.open, args.image_paths)),
+        ))
+        for impath, img in zip(args.image_paths, images):
+            if args.psnr:
+                logger.info("psnr={} ({})".format(psnr(gt_image, img), impath))
 
     else:
         assert_never("tyro already ensures subcommand passed here are valid, this line should never be executed")
 
 
 if __name__ == "__main__":
-    do = tyro.cli(Args, return_unknown_args=True)
-    main(do)
+    args = tyro.cli(Args)
+    main(args)
