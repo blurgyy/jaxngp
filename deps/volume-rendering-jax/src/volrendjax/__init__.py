797f88155d001e4c9857c971bdab05777abd7aff
Gaoyang Zhang <gy@blurgy.xyz>
2023-04-27 12:22:03 +0800

    deps(volrendjax): minor update
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- deps/volume-rendering-jax/src/volrendjax/__init__.py
+++ deps/volume-rendering-jax/src/volrendjax/__init__.py
@@ -1,7 +1,7 @@
-from .packbits_jax import packbits
-from .marching_jax import march_rays, march_rays_inference
-from .morton3d_jax import morton3d, morton3d_invert
-from .integrating_jax import integrate_rays, integrate_rays_inference
+from .packbits import packbits
+from .marching import march_rays, march_rays_inference
+from .morton3d import morton3d, morton3d_invert
+from .integrating import integrate_rays, integrate_rays_inference
 
 
 __all__ = [
