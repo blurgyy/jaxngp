9912128479acfd6a4c54b7a8449484c1ef49a816
Gaoyang Zhang <gy@blurgy.xyz>
2023-05-21 14:32:53 +0800

    opt(hashgrid): use vmap instead of tree_map or loops
    This boosts training/inference speed on the nerf_synthetic dataset by
    ~1.1x and ~1.6x, respectively.
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- models/encoders.py
+++ models/encoders.py
@@ -1,5 +1,4 @@
 import math
-from typing import Any
 
 import chex
 import flax.linen as nn
@@ -63,7 +62,7 @@ class HashGridEncoder(Encoder):
         # Essentially, it is $(n_max / n_min) ** (1/(L - 1))$
         b = math.exp((math.log(self.N_max) - math.log(self.N_min)) / (self.L - 1))
 
-        resolutions, indexing_methods, offsets = [], [], [0]
+        resolutions, use_tiled, offsets = [], [], [0]
         for i in range(self.L):
             res = int(self.N_min * (b**i))
             resolutions.append(res - 1)
@@ -71,9 +70,9 @@ class HashGridEncoder(Encoder):
             n_entries = res ** self.dim
 
             if n_entries <= self.T:
-                indexing_methods.append(self.onebyone)
+                use_tiled.append(True)
             else:
-                indexing_methods.append(self.hashing)
+                use_tiled.append(False)
                 n_entries = self.T
 
             offsets.append(offsets[-1] + n_entries)
@@ -89,151 +88,111 @@ class HashGridEncoder(Encoder):
             self.param_dtype,
         )
 
-        # A list-like object, contents are: [
-        #   (indices_0, pos_scaled_0, vert_pos_0),
-        #   (indices_1, pos_scaled_1, vert_pos_1),
-        #   ...
-        #   (indices_{L-1}, pos_scaled_{L-1}, vert_pos_{L-1})
-        # ], where:
-        # indices [pos.shape[0], 2**dim] (int):
-        #   indices in hash table
-        # pos_scaled [pos.shape[0], 2**dim] (float):
-        #   query points' coordinates scaled to each hierarchies (float)
-        # vert_pos [pos.shape[0], 2**dim, dim] (int):
-        #   vertex coordinates of the enclosing grid cell's vertices (2**dim in total) of each
-        #   query point
-        indices_posscaled_vertpos_tuples = jax.tree_map(
-            lambda index_fn, res: index_fn(pos, self.dim, res, self.T),
-            indexing_methods,
-            resolutions,
+        @vmap_jaxfn_with(in_axes=[0])
+        @vmap_jaxfn_with(in_axes=[0])
+        def make_vert_pos(pos_scaled: jax.Array):
+            # [dim]
+            pos_floored = jnp.floor(pos_scaled).astype(jnp.uint32)
+            # [2**dim, dim]
+            vert_pos = pos_floored[None, :] + cell_vert_offsets[self.dim]
+            return vert_pos.astype(jnp.uint32)
+
+        @vmap_jaxfn_with(in_axes=[0, 0])
+        @vmap_jaxfn_with(in_axes=[None, 0])
+        def make_tiled_indices(res, vert_pos):
+            """(first 2 axes `[L, n_points]` are vmapped away)
+            Inputs:
+                res `uint32` `[L]`: each hierarchy's resolution
+                vert_pos `uint32` `[L, n_points, 2**dim, dim]`: integer positions of grid cell's
+                                                                vertices, of each level
+
+            Returns:
+                indices `[L, n_points, 2**dim]`: grid cell indices of the vertices
+            """
+            # [dim]
+            if self.dim == 2:
+                strides = jnp.stack([res + 1, jnp.ones_like(res)]).T
+            elif self.dim == 3:
+                strides = jnp.stack([(res + 1) ** 2, res + 1, jnp.ones_like(res)]).T
+            else:
+                raise NotImplementedError("{} is only implemented for 2D and 3D data".format(__class__.__name__))
+            # [2**dim]
+            indices = jnp.sum(strides[None, :] * vert_pos, axis=-1)
+            return indices
+
+        @vmap_jaxfn_with(in_axes=[0])
+        @vmap_jaxfn_with(in_axes=[0])
+        def make_hash_indices(vert_pos):
+            """(first 2 axes `[L, n_points]` are vmapped away)
+            Inputs:
+                vert_pos `uint32` `[L, n_points, 2**dim, dim]`: integer positions of grid cell's
+                                                                vertices, of each level
+
+            Returns:
+                indices `[L, n_points, 2**dim]`: grid cell indices of the vertices
+            """
+            # use primes as reported in the paper
+            primes = jnp.asarray([1, 2_654_435_761, 805_459_861], dtype=jnp.uint32)
+            # [2**dim]
+            if self.dim == 2:
+                indices = vert_pos[:, 0] ^ (vert_pos[:, 1] * primes[1])
+            elif self.dim == 3:
+                indices = vert_pos[:, 0] ^ (vert_pos[:, 1] * primes[1]) ^ (vert_pos[:, 2] * primes[2])
+            else:
+                raise NotImplementedError("{} is only implemented for 2D and 3D data".format(__class__.__name__))
+            indices = jnp.mod(indices, self.T)
+            return indices
+
+        @vmap_jaxfn_with(in_axes=(0, 0))
+        @vmap_jaxfn_with(in_axes=(0, 0))
+        def lerp_weights(pos_scaled: jax.Array, vert_pos: jax.Array):
+            """(first 2 axes `[L, n_points]` are vmapped away)
+            Inputs:
+                pos_scaled `float` `[L, n_points, dim]`: coordinates of query points, scaled to the
+                                                         hierarchy in question
+                vert_pos `uint32` `[L, n_points, 2**dim, dim]`: integer coordinates of the grid
+                                                                cells' vertices that enclose each of
+                                                                `pos_scaled`
+
+            Returns:
+                weights `float` `[L, n_points, 2**dim]`: linear interpolation weights for each cell
+                                                         vertex
+            """
+            # [1, dim]
+            pos_offset = pos_scaled[None, :] - vert_pos[0:1, :]
+            # [2**dim, dim]
+            widths = jnp.clip(
+                # cell_vert_offsets: [2**dim, dim]
+                (1 - cell_vert_offsets[self.dim]) + (2 * cell_vert_offsets[self.dim] - 1) * pos_offset,
+                0,
+                1,
+            )
+            # [2**dim]
+            return jnp.prod(widths, axis=-1)
+
+        # [L]
+        resolutions = jnp.asarray(resolutions, dtype=jnp.uint32)
+        # [L, n_points, dim]
+        pos_scaled = pos[None, :, :] * resolutions[:, None, None]
+        # [L, n_points, 2**dim, dim]
+        vert_pos = make_vert_pos(pos_scaled)
+        # [L, n_points, 2**dim]
+        indices = jnp.where(
+            jnp.asarray(use_tiled, dtype=jnp.bool_)[:, None, None],
+            make_tiled_indices(resolutions, vert_pos),
+            make_hash_indices(vert_pos),
         )
 
-        # indices [L, pos.shape[0], 2**dim] (int):
-        #   indices in hash table
-        # pos_scaled [L, pos.shape[0], 2**dim] (float):
-        #   query points' coordinates scaled to each hierarchies (float)
-        # vert_pos [L, pos.shape[0], 2**dim, dim] (int):
-        #   vertex coordinates of the enclosing grid cell's vertices (2**dim in total) of each
-        #   query point
-        indices, pos_scaled, vert_pos = map(jnp.stack, zip(*indices_posscaled_vertpos_tuples))
-
-        # add offsets for each hash grid level
-        indices += jnp.asarray(offsets[:-1]).reshape((self.L, 1, 1))
-
-        # [L, pos.shape[0], 2**dim, F]
+        # [L, n_points, 2**dim, F]
         vert_latents = latents[indices]
-        # [L, pos.shape[0], 2**dim]
-        # NOTE:
-        #   need to set out_axes=1 to keep the output batch dimension on the 1-th axis
-        vert_weights = self.lerp_weights(pos_scaled, vert_pos, self.dim)
+        # [L, n_points, 2**dim]
+        vert_weights = lerp_weights(pos_scaled, vert_pos)
 
         # [L, pos.shape[0], F]
         encodings = (vert_latents * vert_weights[..., None]).sum(axis=-2)
         # [pos.shape[0], L*F]
         encodings = encodings.transpose(1, 0, 2).reshape(-1, self.L * self.F)
         return encodings
-
-
-    # PERF:
-    #   Make sure to jit the vmapped function, and NOT the other way around (always use jit as the
-    #   outmost decorator).
-    #   The @staticmethod decorator here doesn't degrade performance, but can only be add at top.
-    @staticmethod
-    @jit_jaxfn_with(static_argnames=["dim"])
-    @vmap_jaxfn_with(in_axes=(1, 1, None), out_axes=1)
-    def lerp_weights(pos_scaled: jax.Array, vert_pos: jax.Array, dim: int):
-        """
-        Inputs:
-            pos_scaled [L, ..., dim]: coordinates of query points, scaled to the hierarchy in question
-            vert_pos [L, ..., 2**dim, dim]: integer coordinates of the grid cells' vertices that enclose each of `pos_scaled`
-            dim int: space dimension
-
-        Returns:
-            weights [L, ..., 2**dim]: linear interpolation weights w.r.t. each cell vertex
-        """
-        chex.assert_type([pos_scaled, vert_pos, dim], [float, int, int])
-        chex.assert_axis_dimension(pos_scaled, -1, dim)
-        chex.assert_axis_dimension(vert_pos, -2, 2**dim)
-        chex.assert_axis_dimension(vert_pos, -1, dim)
-        chex.assert_scalar(dim)
-        # [L, ..., 1, dim]
-        pos_offset = pos_scaled[..., None, :] - vert_pos[..., 0:1, :]
-        # [L, ..., 2**dim, dim]
-        widths = jnp.clip(
-            # cell_vert_offsets: [2**dim, dim]
-            (1 - cell_vert_offsets[dim]) + (2 * cell_vert_offsets[dim] - 1) * pos_offset,
-            0,
-            1,
-        )
-        # [L, ..., 2**dim]
-        return jnp.prod(widths, axis=-1)
-
-    @staticmethod
-    @jit_jaxfn_with(static_argnames=["dim", "res"])
-    @vmap_jaxfn_with(in_axes=(0, None, None, None))
-    def onebyone(pos: jax.Array, dim: int, res: int, _: Any):
-        """
-        Inputs:
-            pos `[..., dim]`: spatial positions of the query points
-            dim `int`: dimension of input data
-            res `int`: resolution of the hierarchy in question
-
-        Returns:
-            indices [..., 2**dim]: indices of the grid cell's vertices in the hash table
-            pos_scaled [..., dim]: query points' coordinates scaled to each hierarchies (float)
-            vert_pos [..., 2**dim, dim]: positions of the grid cell's vertices in the input space
-        """
-        chex.assert_type(pos, float)
-        chex.assert_axis_dimension(pos, -1, dim)
-        # [..., dim]
-        pos_scaled = pos * res
-        # [..., dim]
-        pos_floored = jnp.floor(pos_scaled).astype(int)
-        # [..., 2**dim, dim]
-        vert_pos = pos_floored[..., None, :] + cell_vert_offsets[dim]
-        # [..., 2**dim]
-        if dim == 2:
-            strides = jnp.asarray([(res + 1), 1], dtype=jnp.uint32)
-        elif dim == 3:
-            strides = jnp.asarray([(res + 1) ** 2, (res + 1), 1], dtype=jnp.uint32)
-        else:
-            raise NotImplementedError("{} is only implemented for 2D and 3D data".format(__class__.__name__))
-        indices = jnp.sum(strides[None, :] * vert_pos, axis=-1)
-        return indices, pos_scaled, vert_pos
-
-    @staticmethod
-    @jit_jaxfn_with(static_argnames=["dim", "res", "T"])
-    @vmap_jaxfn_with(in_axes=(0, None, None, None))
-    def hashing(pos: jax.Array, dim: int, res: int, T: int):
-        """
-        Inputs:
-            pos [..., dim]: spatial positions of the query points
-            dim `int`: dimension of input data
-            res int: resolution of the hierarchy in question
-
-        Returns:
-            indices [..., 2**dim]: indices of the grid cell's vertices in the hash table
-            vert_pos [..., 2**dim, dim]: positions of the grid cell's vertices in the input space
-        """
-        chex.assert_type(pos, float)
-        chex.assert_axis_dimension(pos, -1, dim)
-        # [..., dim]
-        pos_scaled = pos * res
-        # [..., dim]
-        pos_floored = jnp.floor(pos_scaled).astype(int)
-        # [..., 2**dim, dim]
-        vert_pos = pos_floored[..., None, :] + cell_vert_offsets[dim]
-        vert_pos = vert_pos.astype(jnp.uint32)
-        # use primes as reported in the paper
-        primes = jnp.asarray([1, 2_654_435_761, 805_459_861], dtype=jnp.uint32)
-        # [..., 2**dim]
-        if dim == 2:
-            indices = vert_pos[..., 0] ^ (vert_pos[..., 1] * primes[1])
-        elif dim == 3:
-            indices = vert_pos[..., 0] ^ (vert_pos[..., 1] * primes[1]) ^ (vert_pos[..., 2] * primes[2])
-        else:
-            raise NotImplementedError("{} is only implemented for 2D and 3D data".format(__class__.__name__))
-        return jnp.mod(indices, T), pos_scaled, vert_pos
 
 
 @empty_impl
